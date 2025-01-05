b959422f95b0b8cc2717497ba5171435ec87158c
moalmatry <moalmatry2000@gmail.com>
2025-01-05 16:36:46 +0200

    feat: add appwrite auth features

---
+++ client/lib/appwrite.ts
@@ -0,0 +1,72 @@
+import { Client, Avatars, Account, OAuthProvider } from "react-native-appwrite";
+import * as Linking from "expo-linking";
+export const config = {
+  platform: "com.restate",
+  endpoint: process.env.EXPO_PUBLIC_ENDPOINT!,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
+};
+import { openAuthSessionAsync } from "expo-web-browser";
+
+export const client = new Client();
+
+client
+  .setEndpoint(config.endpoint)
+  .setProject(config.projectId)
+  .setPlatform(config.platform);
+
+export const avatar = new Avatars(client);
+export const account = new Account(client);
+
+export async function login() {
+  try {
+    const redirectUri = Linking.createURL("/");
+    const response = await account.createOAuth2Token(
+      OAuthProvider.Google,
+      redirectUri
+    );
+    if (!response) throw new Error("Failed to login");
+
+    const browserResult = await openAuthSessionAsync(
+      response.toString(),
+      redirectUri
+    );
+    if (browserResult.type !== "success") throw new Error("Failed to login");
+
+    const url = new URL(browserResult.url);
+    const secret = url.searchParams.get("secret")?.toString();
+    const userId = url.searchParams.get("userId")?.toString();
+
+    if (!secret || !userId) throw new Error("failed to login");
+    const session = await account.createSession(userId, secret);
+
+    if (!session) throw new Error("Failed to create a session");
+    return true;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function logout() {
+  try {
+    await account.deleteSession("current");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getUser() {
+  try {
+    const response = await account.get();
+
+    if (response.$id) {
+      const userAvatar = avatar.getInitials(response.name);
+
+      return {
+        ...response,
+        avatar: userAvatar.toString(),
+      };
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
